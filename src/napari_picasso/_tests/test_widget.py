6a1cbd843932b03c89d37dce7676412b971a2daa
Kunal Pandit <kpandit8387@gmail.com>
2022-05-02 09:09:19 -0400

    remove magic widget test

--- src/napari_picasso/_tests/test_widget.py
+++ src/napari_picasso/_tests/test_widget.py
@@ -21,16 +21,16 @@ def test_picasso_widgete(make_napari_viewer, capsys):
     print(captured.out)
     assert captured.out == "napari has 1 layers\n"
 
-def test_example_magic_widget(make_napari_viewer, capsys):
-    viewer = make_napari_viewer()
-    layer = viewer.add_image(np.random.random((100, 100)))
-
-    # this time, our widget will be a MagicFactory or FunctionGui instance
-    my_widget = example_magic_widget()
-
-    # if we "call" this object, it'll execute our function
-    my_widget(viewer.layers[0])
-
-    # read captured output and check that it's as we expected
-    captured = capsys.readouterr()
-    assert captured.out == f"you have selected {layer}\n"
+# def test_example_magic_widget(make_napari_viewer, capsys):
+#     viewer = make_napari_viewer()
+#     layer = viewer.add_image(np.random.random((100, 100)))
+#
+#     # this time, our widget will be a MagicFactory or FunctionGui instance
+#     my_widget = example_magic_widget()
+#
+#     # if we "call" this object, it'll execute our function
+#     my_widget(viewer.layers[0])
+#
+#     # read captured output and check that it's as we expected
+#     captured = capsys.readouterr()
+#     assert captured.out == f"you have selected {layer}\n"
