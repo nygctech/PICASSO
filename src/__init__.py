41daff449a69a1ade8810bc67329e5a175490d52
Kunal Pandit <kpandit8387@gmail.com>
2023-01-13 11:29:59 -0500

    Merge branch 'main' of https://github.com/nygctech/PICASSO

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+
+try:
+    from _version import version as __version__
+    from _version import version_tuple
+except ImportError:
+    __version__ = "unknown version"
+    version_tuple = (0, 0, "unknown version")
