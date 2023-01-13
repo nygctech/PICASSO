6d2e5eb290a677cebdcddb18e655162046f04581
Kunal Pandit <kpandit8387@gmail.com>
2023-01-13 13:04:47 -0500

    fix typo

--- src/picasso/__init__.py
+++ src/picasso/__init__.py
@@ -3,7 +3,7 @@ try:
     from _version import version as __version__
     from _version import version_tuple
 except ImportError:
-    __version__ = 0.3.0
+    __version__ = '0.3.0'
     version_tuple = (0, 3, 0)
 
 
