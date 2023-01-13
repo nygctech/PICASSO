46bd734279ac06c4b6458af11a41099311a14b9d
Kunal Pandit <kpandit8387@gmail.com>
2023-01-13 12:19:56 -0500

    hardcode backup version

--- src/picasso/__init__.py
+++ src/picasso/__init__.py
@@ -3,8 +3,8 @@ try:
     from _version import version as __version__
     from _version import version_tuple
 except ImportError:
-    __version__ = "unknown version"
-    version_tuple = (0, 0, "unknown version")
+    __version__ = 0.3.0
+    version_tuple = (0, 3, 0)
 
 
 
